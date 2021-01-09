46ee63d9fa916553b40518217cd7a5e6d309db47
benjaminhuanghuang <benjaminhuanghuang@gmail.com>
2021-01-08 23:31:07 -0800

    start 25

---
+++ 25-flicker-2/graphics.c
@@ -0,0 +1,214 @@
+#include "os.h"
+
+void init_palette(void)
+{
+  // value of 16 colors
+  static unsigned char table_rgb[16 * 3] = {
+      0x00, 0x00, 0x00, /*  0:黒 */
+      0xff, 0x00, 0x00, /*  1:明るい赤 */
+      0x00, 0xff, 0x00, /*  2:明るい緑 */
+      0xff, 0xff, 0x00, /*  3:明るい黄色 */
+      0x00, 0x00, 0xff, /*  4:明るい青 */
+      0xff, 0x00, 0xff, /*  5:明るい紫 */
+      0x00, 0xff, 0xff, /*  6:明るい水色 */
+      0xff, 0xff, 0xff, /*  7:白 */
+      0xc6, 0xc6, 0xc6, /*  8:明るい灰色 */
+      0x84, 0x00, 0x00, /*  9:暗い赤 */
+      0x00, 0x84, 0x00, /* 10:暗い緑 */
+      0x84, 0x84, 0x00, /* 11:暗い黄色 */
+      0x00, 0x00, 0x84, /* 12:暗い青 */
+      0x84, 0x00, 0x84, /* 13:暗い紫 */
+      0x00, 0x84, 0x84, /* 14:暗い水色 */
+      0x84, 0x84, 0x84  /* 15:暗い灰色 */
+  };
+
+  set_palette(0, 15, table_rgb);
+  return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+  int i, eflags;
+  // preserve eflags
+  eflags = io_load_eflags();
+  // disable interupt
+  io_cli();
+  // write port
+  io_out8(0x03c8, start); //set  palette number
+  for (i = start; i <= end; i++)
+  {
+    io_out8(0x03c9, rgb[0] / 4);
+    io_out8(0x03c9, rgb[1] / 4);
+    io_out8(0x03c9, rgb[2] / 4);
+
+    rgb += 3;
+  }
+  // recover eflags
+  io_store_eflags(eflags);
+  return;
+}
+
+void boxfill8(unsigned char *vram, int xsize, unsigned char c,
+              int x0, int y0, int x1, int y1)
+{
+  int x, y;
+  for (y = y0; y <= y1; y++)
+    for (x = x0; x <= x1; x++)
+    {
+      vram[y * xsize + x] = c;
+    }
+}
+
+void init_mouse_cursor(unsigned char *mouse, char bc)
+{
+  static char cursor[16][16] = {
+      "**************..",
+      "*OOOOOOOOOOO*...",
+      "*OOOOOOOOOO*....",
+      "*OOOOOOOOO*.....",
+      "*OOOOOOOO*......",
+      "*OOOOOOO*.......",
+      "*OOOOOOO*.......",
+      "*OOOOOOOO*......",
+      "*OOOO**OOO*.....",
+      "*OOO*..*OOO*....",
+      "*OO*....*OOO*...",
+      "*O*......*OOO*..",
+      "**........*OOO*.",
+      "*..........*OO0*",
+      "............*OO*",
+      ".............***"};
+
+  int x, y;
+  for (y = 0; y < 16; y++)
+  {
+    for (x = 0; x < 16; x++)
+    {
+      if (cursor[y][x] == '*')
+      {
+        mouse[y * 16 + x] = COL8_000000;
+      }
+      if (cursor[y][x] == 'O')
+      {
+        mouse[y * 16 + x] = COL8_FFFFFF;
+      }
+      if (cursor[y][x] == '.')
+      {
+        mouse[y * 16 + x] = bc;
+      }
+    }
+  }
+}
+
+void putblock(unsigned char *vram, int vxsize, int pxsize, int pysize, int px0,
+              int py0, unsigned char *buf, int bxsize)
+{
+  int x, y;
+  for (y = 0; y < pysize; y++)
+  {
+    for (x = 0; x < pxsize; x++)
+    {
+      vram[(py0 + y) * vxsize + (px0 + x)] = buf[y * bxsize + x];
+    }
+  }
+}
+
+void showFont8(unsigned char *vram, int xsize, int x, int y, char c, char *font)
+{
+  int i;
+	unsigned char *p;
+  char d /* data */;
+	for (i = 0; i < 16; i++) {
+		p = vram + (y + i) * xsize + x;	
+    d = font[i];
+		if ((d & 0x80) != 0) { p[0] = c; }
+		if ((d & 0x40) != 0) { p[1] = c; }
+		if ((d & 0x20) != 0) { p[2] = c; }
+		if ((d & 0x10) != 0) { p[3] = c; }
+		if ((d & 0x08) != 0) { p[4] = c; }
+		if ((d & 0x04) != 0) { p[5] = c; } 
+		if ((d & 0x02) != 0) { p[6] = c; }
+		if ((d & 0x01) != 0) { p[7] = c; }
+	}
+	return;
+}
+
+void showString(unsigned char *vram, int xsize, int x, int y, char color, unsigned char *s)
+{
+  for (; *s != 0x00; s++)
+  {
+    showFont8(vram, xsize, x, y, color, systemFont + *s * 16);
+    x += 8;
+  }
+}
+
+
+void draw_desktop(unsigned char *vram, int x, int y)
+{
+	boxfill8(vram, x, COL8_008484,  0,     0,      x -  1, y - 29);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 28, x -  1, y - 28);
+	boxfill8(vram, x, COL8_FFFFFF,  0,     y - 27, x -  1, y - 27);
+	boxfill8(vram, x, COL8_C6C6C6,  0,     y - 26, x -  1, y -  1);
+
+	boxfill8(vram, x, COL8_FFFFFF,  3,     y - 24, 59,     y - 24);
+	boxfill8(vram, x, COL8_FFFFFF,  2,     y - 24,  2,     y -  4);
+	boxfill8(vram, x, COL8_848484,  3,     y -  4, 59,     y -  4);
+	boxfill8(vram, x, COL8_848484, 59,     y - 23, 59,     y -  5);
+	boxfill8(vram, x, COL8_000000,  2,     y -  3, 59,     y -  3);
+	boxfill8(vram, x, COL8_000000, 60,     y - 24, 60,     y -  3);
+
+	boxfill8(vram, x, COL8_848484, x - 47, y - 24, x -  4, y - 24);
+	boxfill8(vram, x, COL8_848484, x - 47, y - 23, x - 47, y -  4);
+	boxfill8(vram, x, COL8_FFFFFF, x - 47, y -  3, x -  4, y -  3);
+	boxfill8(vram, x, COL8_FFFFFF, x -  3, y - 24, x -  3, y -  3);
+	return;
+}
+
+void make_window8(unsigned char *buf, int xsize, int ysize, char *title)
+{
+	static char closebtn[14][16] = {
+		"OOOOOOOOOOOOOOO@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQQQ@@QQQQQ$@",
+		"OQQQQQ@@@@QQQQ$@",
+		"OQQQQ@@QQ@@QQQ$@",
+		"OQQQ@@QQQQ@@QQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"OQQQQQQQQQQQQQ$@",
+		"O$$$$$$$$$$$$$$@",
+		"@@@@@@@@@@@@@@@@"
+	};
+	int x, y;
+	char c;
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         xsize - 1, 0        );
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         xsize - 2, 1        );
+	boxfill8(buf, xsize, COL8_C6C6C6, 0,         0,         0,         ysize - 1);
+	boxfill8(buf, xsize, COL8_FFFFFF, 1,         1,         1,         ysize - 2);
+	boxfill8(buf, xsize, COL8_848484, xsize - 2, 1,         xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, xsize - 1, 0,         xsize - 1, ysize - 1);
+	boxfill8(buf, xsize, COL8_C6C6C6, 2,         2,         xsize - 3, ysize - 3);
+	boxfill8(buf, xsize, COL8_000084, 3,         3,         xsize - 4, 20       );
+	boxfill8(buf, xsize, COL8_848484, 1,         ysize - 2, xsize - 2, ysize - 2);
+	boxfill8(buf, xsize, COL8_000000, 0,         ysize - 1, xsize - 1, ysize - 1);
+	showString(buf, xsize, 24, 4, COL8_FFFFFF, title);
+	for (y = 0; y < 14; y++) {
+		for (x = 0; x < 16; x++) {
+			c = closebtn[y][x];
+			if (c == '@') {
+				c = COL8_000000;
+			} else if (c == '$') {
+				c = COL8_848484;
+			} else if (c == 'Q') {
+				c = COL8_C6C6C6;
+			} else {
+				c = COL8_FFFFFF;
+			}
+			buf[(5 + y) * xsize + (xsize - 21 + x)] = c;
+		}
+	}
+	return;
+}
